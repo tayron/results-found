6a7fdce7cd663f9f739044dbf8171e9d93e95647
Tayon Miranda <dev@tayron.com.br>
2016-11-14 11:34:44 -0200

    DELETE 	- Removendo arquivo teste.php

--- teste.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'ResultsFound.php';
-
-$texto = 'pedro321cardoso@teste.com.br';
-$procurar = 'pedro';
-
-$resultsFound = new ResultsFound();
-echo $resultsFound->markText($texto, $procurar);
